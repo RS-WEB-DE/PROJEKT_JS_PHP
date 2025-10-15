e99160ae3b058e96909447bcbfdcdc22c30ff5ec
rxadmin <roman.sakhnin@gmail.com>
2025-10-15 13:52:53 +0200

    change config

--- api/config.php
+++ api/config.php
@@ -1,11 +1,13 @@
 <?php
 
+$env = parse_ini_file(__DIR__ . '/../.env');
+
 return [
-  'host' => 'localhost',
-  'dbname' => 'watchcraft_db',
-  'user' => 'root',
-  'pass' => '',
-  'charset' => 'utf8mb4',
+  'host' => $env['DB_HOST'] ?? '127.0.0.1',
+  'dbname' => $env['DB_NAME'] ?? '',
+  'user' => $env['DB_USER'] ?? '',
+  'pass' => $env['DB_PASS'] ?? '',
+  'charset' => $env['DB_CHARSET'] ?? 'utf8mb4'
 ];
 
 
